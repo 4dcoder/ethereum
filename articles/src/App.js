2ec5ef7cc5f4929166d3a90fc04ef27362f7b2e3
“Rawad Rifai” <rawad.rifai@gmail.com>
2017-10-29 05:11:42 -0700

    no idea it just showed me a change

--- articles/src/App.js
+++ articles/src/App.js
@@ -6,7 +6,32 @@ import Input from './Input'
 import './App.css';
 
 class App extends React.Component {
+
+  constructor() {
+    this.setState({tag: "init"});
+    App.initWeb3();
+
+  }
+
+  initWeb3() {
+    this.setState({tag: "init web3"});
+    // Initialize web3 and set the provider to the testRPC.
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+   // App.displayAccountInfo();
+   // return App.initContract();
+  }
+
+
+
   render() {
+    
     return (
       
       <div>
@@ -14,10 +39,11 @@ class App extends React.Component {
 
         <Input/>
         <br/>
-
+        <label>aaa {this.state.tag}</label>
       </div>
     );
   }
 }
+
 
 export default App;
