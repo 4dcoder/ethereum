173928886041b1b2e64c752b50e5285a061acc79
rawadrifai <rawad.rifai@gmail.com>
2017-11-07 16:58:55 -0800

    deploy to github pages

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,4 +1,4 @@
-rsync -r ChainList/src/ docs/
+rsync -r ChainList/build_webpack/ docs/
 rsync ChainList/build/contracts/ChainList.json docs/
 git add .
 git commit -m "Adding frontend files to Github Pages"
